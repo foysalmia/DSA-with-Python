5c98f952390e4bceebcc90b025216b8b1234e698
Foysal Ahmed <mfoysal314@gmail.com>
2023-12-11 01:47:40 +0600

    append added

--- linkedlist.py
+++ linkedlist.py
@@ -10,6 +10,17 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
     
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+
+        self.tail = new_node 
+        self.length += 1
+        return True
+
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -20,7 +31,14 @@ class LinkedList:
             temp = temp.next
 
 first_linked_list = LinkedList(4)
+
+first_linked_list.append(5)
+first_linked_list.append(6)
+first_linked_list.append(7)
+
 print("head = ",first_linked_list.head.value)
 print("tail = ",first_linked_list.tail.value)
-print("length = ",first_linked_list.length)
+
+
 first_linked_list.print_list()
+print("length = ",first_linked_list.length)
