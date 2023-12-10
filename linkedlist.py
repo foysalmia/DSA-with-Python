38027ec7728ceb05d7f19a079c2f6fe8837d4ab7
Foysal Ahmed <mfoysal314@gmail.com>
2023-12-11 01:35:27 +0600

    print list added in this linked list

--- linkedlist.py
+++ linkedlist.py
@@ -6,11 +6,21 @@ class Node:
 class LinkedList:
     def __init__(self,value) -> None:
         new_node = Node(value)
-        self.head = new_node.value
-        self.tail = new_node.value
+        self.head = new_node
+        self.tail = new_node
         self.length = 1
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            if temp.next == None:
+                print(temp.value)
+            else:
+                print(temp.value,end='->')
+            temp = temp.next
 
 first_linked_list = LinkedList(4)
-print("head = ",first_linked_list.head)
-print("tail = ",first_linked_list.tail)
+print("head = ",first_linked_list.head.value)
+print("tail = ",first_linked_list.tail.value)
 print("length = ",first_linked_list.length)
+first_linked_list.print_list()
