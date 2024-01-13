2e2644ef6189f35da965e46bb0dc7dbb7c47d236
Foysal Ahmed <mfoysal314@gmail.com>
2024-01-13 22:41:14 +0600

    prepend added in linkedlist

--- linkedlist.py
+++ linkedlist.py
@@ -10,6 +10,13 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
     
+    def prepend(self,value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+        return True
+
     def append(self,value):
         new_node = Node(value)
         if self.head is None:
@@ -46,12 +53,21 @@ class LinkedList:
                 print(temp.value,end='->')
             temp = temp.next
 
-first_linked_list = LinkedList(4)
 
-
-first_linked_list.pop()
+first_linked_list = LinkedList(2)
+first_linked_list.append(3)
+print("Before Prepend".center(20))
+print("Head = ",first_linked_list.head.value)
+print("Tail = ",first_linked_list.tail.value)
+print("Length = ",first_linked_list.length)
+print("Linked List => ", end="")
 first_linked_list.print_list()
-print("length = ",first_linked_list.length)
 
-print("head = ",first_linked_list.head)
-print("tail = ",first_linked_list.tail)
+
+print("After Prepend".center(20))
+first_linked_list.prepend(5)
+print("Head = ",first_linked_list.head.value)
+print("Tail = ",first_linked_list.tail.value)
+print("Length = ",first_linked_list.length)
+print("Linked List => ", end="")
+first_linked_list.print_list()
