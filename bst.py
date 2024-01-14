21dc70fbe9aed1a8ab3cc77efe9bd79f48e9c814
Foysal Ahmed <mfoysal314@gmail.com>
2024-01-15 00:29:26 +0600

    binary Search Tree added

---
+++ bst.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self) -> None:
+        self.root = None
+
+
+
+
+my_bst = BinarySearchTree()
+print(my_bst.root)
