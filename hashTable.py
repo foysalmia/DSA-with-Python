a6e1187dbef0f9bc4b7ec98346ee54b9bf8fec33
Foysal Ahmed <mfoysal314@gmail.com>
2024-01-16 00:02:12 +0600

    set and get method added in hash table

--- hashTable.py
+++ hashTable.py
@@ -12,6 +12,23 @@ class HashTable:
         for i,val in enumerate(self.data_map):
             print(i,":",val)
 
+    def set_item(self,key,value):
+        index = self._hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key,value])
+    
+    def get(self,key):
+        index = self._hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
 
 my_hash = HashTable(7)
+my_hash.set_item('nuts',1400)
+my_hash.set_item('pipe',1000)
+my_hash.set_item('longs',500)
 my_hash.print_table()
+print(my_hash.get("mike"))
