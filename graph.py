9a4d951214d6c680cc85c8d16e958862b32c2784
Foysal Ahmed <mfoysal314@gmail.com>
2024-01-16 01:52:06 +0600

    other methods added in graph

--- graph.py
+++ graph.py
@@ -8,10 +8,48 @@ class Graph:
             return True
         return False
     
+    def add_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+    def remove_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+    
+    def remove_vertex(self,vertex):
+        if vertex in self.adj_list.keys():
+            for other in self.adj_list[vertex]:
+                self.adj_list[other].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
     def print_graph(self):
         for vertex in self.adj_list:
             print(vertex,":",self.adj_list[vertex])
 
 my_graph = Graph()
 my_graph.add_vertex("A")
+my_graph.add_vertex("B")
+my_graph.add_vertex("C")
+my_graph.add_vertex("D")
+my_graph.add_edge("B","A")
+my_graph.add_edge("C","A")
+my_graph.add_edge("D","A")
+my_graph.add_edge("D","B")
+my_graph.add_edge("D","C")
+my_graph.print_graph()
+print()
+print("After remove Vertex".center(20,"*"))
+print()
+my_graph.remove_vertex("D")
 my_graph.print_graph()
